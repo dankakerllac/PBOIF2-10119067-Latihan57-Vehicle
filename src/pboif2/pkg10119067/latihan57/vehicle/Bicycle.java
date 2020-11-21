f08e32805d7d64b87a6770f923dfa9e8d5510871
User <User@DESKTOP-6N6EHH6>
2020-11-21 22:51:57 +0700

    PBOIF2-10119067

---
+++ src/pboif2/pkg10119067/latihan57/vehicle/Bicycle.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119067.latihan57.vehicle;
+
+/**
+ *
+ * @author Danhago
+ * NAMA     : Andreas Danhago P.I
+ * KELAS    : IF2
+ * NIM      : 10119082
+ * Deskripsi: Vehicle
+ */
+public class Bicycle extends Vehicle{
+    private int myGearCount;
+
+    public Bicycle() {
+        System.out.println("Bicycle");
+    }
+
+    public int getMyGearCount() {
+        return myGearCount;
+    }
+
+    public void setMyGearCount(int myGearCount) {
+        this.myGearCount = myGearCount;
+    }
+    
+}
